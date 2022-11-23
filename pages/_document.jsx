400429b1a70f768237f63adb10e0bfc128f5f6b0
BackGyeHoon <bk88888@naver.com>
2022-11-23 10:58:04 +0900

    seo 최적화 작업

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,12 +6,13 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>법률사무소 정문</title>
           <meta
             name="keywords"
             content="시흥, 안산, 공단, 시화, 화성, 수원, 안양, 군포, 인천, 청라, 배곧, 광명, 부천, 변호사, 법률사무소, 법무법인, lawfirm, lawyer, law, 문일식, 전문, 자문, 변리사, 세무사, 행정사, 법무사, 검찰, 검찰청, 감사원, 국세청, 대한법률구조공단, 출신, 민사, 부동산, 임대차, 손해배상, 대여금, 약정금, 물품대금, 미수금, 추심, 가압류, 압류, 강제집행, 경매, 등기, 공탁, 공증, 내용증명, 법률상담, 건설, 임대차, 부동산, 동업, 경영권, 분쟁, 스타트업, 주주총회, 이사회, 형사, 사기, 공갈, 협박, 모욕, 명예훼손, 보이스피싱, 성범죄, 성추행, 강제추행, 성폭행, 성폭력, 피해자, 가해자, 강간, 준강간, 마약, 음주, 교통사고, 고소, 고발, 고소장, 재심, 행정, 학교폭력, 출입국, 외국인, foreigner, 사무소, 추방명령, deportation, 보호, 일시, 해제, 조세, 조세심판원, 조세심판, 불복, 이의, 세금, 부가가치세, 경정, 청구, 법인세, 소득세, 종합소득세, 양도세, 지방세, 산재, 근로복지공단, 사고, 유족, 연금, 급여, 장해, 등급, 요양, 요양비, 휴업, 재요양, 추가상병, 불승인, 간병료, 노동, 부당, 해고, 행위, 차별, 시정, 근로기준법, 위반, 임금, 퇴직금, 해고, 환경, 악취, 소음, 진동, 먼지, 일조방해, 일조저해, 정신적, 피해, 소청, 징계, 해임, 파면, 정직, 강등, 공무원, 공정거래, 하도급, 가맹, 유통, 할부, 거래, 전자상거래, 의료, 사고, 개명, 상속, 포기, 한정승인, 유언, 이혼, 재산분할, 양육권, 친권, 양육비, 분양, 계약, 해제, 취소, 컨설팅, 시흥변호사, 안산변호사, 배곧변호사"
           />
           <meta
-            name="Description"
+            name="description"
             content="검찰청/감사원/국세청 출신 문일식 변호가 직접 상담부터 승소까지 책임집니다"
           />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
@@ -19,7 +20,18 @@ export default class CustomDocument extends Document {
             property="og:description"
             content="검찰청/감사원/국세청 출신 문일식 변호가 직접 상담부터 승소까지 책임집니다"
           />
+          <meta property="og:title" content="법률사무소 정문" />
           <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://jeongmoon.com/" />
+          <meta
+            property="og:image"
+            content="https://jeongmoon.com/icons/icon-logo.png"
+          />
+          <meta property="og:article:author" content="법률사무소 정문" />
+          <meta
+            property="og:description"
+            content="검찰청/감사원/국세청 출신 문일식 변호가 직접 상담부터 승소까지 책임집니다"
+          />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@" />
           <meta
